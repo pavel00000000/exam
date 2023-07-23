c732c002dc5c62a211e0db5e7e0bc1094bffbf41
Дейнека Павлик <freddik1990@gmail.com>
2023-07-23 12:13:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿//Создать приложение «Словари».
+//Основная задача проекта: хранить словари на разных языках и разрешать пользователю находить перевод нужного слова или фразы.
+//Интерфейс приложения должен предоставлять такие возможности:
+//■ Создавать словарь. При создании нужно указать тип словаря.
+//Например, англо-русский или русско-английский.
+//■ Добавлять слово и его перевод в уже существующий словарь. Так как у слова может быть несколько переводов, необходимо поддерживать возможность создания
+//нескольких вариантов перевода.
+//■ Заменять слово или его перевод в словаре.
+//■ Удалять слово или перевод. Если удаляется слово, все его переводы удаляются
+//вместе с ним. Нельзя удалить перевод слова, если это последний вариант перевода.
+//■ Искать перевод слова.
+//■ Словари должны храниться в файлах.
+//■ Слово и варианты его переводов можно экспортировать в отдельный файл результата.
+//■ При старте программы необходимо показывать меню для работы с программой.
+//Если выбор пункта меню открывает подменю, то тогда в нем требуется предусмотреть возможность возврата в предыдущее меню.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using EnglishRussian;
+using RussianEnglish;
+using RussianFrench;
+using FrenchRussian;
+
+class Program
+{
+    static Dictionary<string, Dictionary<string, List<string>>> dictionaries = new Dictionary<string, Dictionary<string, List<string>>>();
+
+    static void Main()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Главное меню ===");
+            Console.WriteLine("1. Создать англо-русский словарь");
+            Console.WriteLine("2. Создать русско-английский словарь");
+            Console.WriteLine("3. Создать русско-французкий словарь");
+            Console.WriteLine("4. Создать французко-русский словарь");
+            Console.WriteLine("0. Выйти");
+            Console.Write("Выберите действие: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    EnglishRussianDictionary englishRussianDictionary = new EnglishRussianDictionary();
+                    bool exit1 = false;
+                    while (!exit1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("=== Англо-русский словарь ===");
+                        Console.WriteLine("1. Добавить слово и перевод");
+                        Console.WriteLine("2. Заменить слово или перевод");
+                        Console.WriteLine("3. Удалить слово или перевод");
+                        Console.WriteLine("4. Найти перевод слова");
+                        Console.WriteLine("5. Экспорт слов и переводов");
+                        Console.WriteLine("0. Выйти");
+                        Console.Write("Выберите действие: ");
+                        string choice1 = Console.ReadLine();
+
+                        switch (choice1)
+                        {
+                            case "1":
+                                englishRussianDictionary.AddWordTranslation();
+                                break;
+                            case "2":
+                                englishRussianDictionary.ReplaceWordTranslation();
+                                break;
+                            case "3":
+                                englishRussianDictionary.DeleteWordTranslation();
+                                break;
+                            case "4":
+                                englishRussianDictionary.SearchTranslation();
+                                break;
+                            case "5":
+                                englishRussianDictionary.ExportWords();
+                                break;
+                            case "0":
+                                exit1 = true;
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                                break;
+                        }
+                    }
+                    break;
+                case "2":
+                    RussianEnglishDictionary russianEnglishDictionary = new RussianEnglishDictionary();
+                    bool exit2 = false;
+                    while (!exit2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("=== Русско-английский словарь ===");
+                        Console.WriteLine("1. Добавить слово и перевод");
+                        Console.WriteLine("2. Заменить слово или перевод");
+                        Console.WriteLine("3. Удалить слово или перевод");
+                        Console.WriteLine("4. Найти перевод слова");
+                        Console.WriteLine("5. Экспорт слов и переводов");
+                        Console.WriteLine("0. Выйти");
+                        Console.Write("Выберите действие: ");
+                        string choice2 = Console.ReadLine();
+
+                        switch (choice2)
+                        {
+                            case "1":
+                                russianEnglishDictionary.AddWordTranslation();
+                                break;
+                            case "2":
+                                russianEnglishDictionary.ReplaceWordTranslation();
+                                break;
+                            case "3":
+                                russianEnglishDictionary.DeleteWordTranslation();
+                                break;
+                            case "4":
+                                russianEnglishDictionary.SearchTranslation();
+                                break;
+                            case "5":
+                                russianEnglishDictionary.ExportWords();
+                                break;
+                            case "0":
+                                exit2 = true;
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                                break;
+                        }
+                    }
+                    break;
+                case "3":
+                    RussianFrenchDictionary RussianFrenchDictionary = new RussianFrenchDictionary();
+                    bool exit3 = false;
+                    while (!exit3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("=== Русскко - французкий словарь ===");
+                        Console.WriteLine("1. Добавить слово и перевод");
+                        Console.WriteLine("2. Заменить слово или перевод");
+                        Console.WriteLine("3. Удалить слово или перевод");
+                        Console.WriteLine("4. Найти перевод слова");
+                        Console.WriteLine("5. Экспорт слов и переводов");
+                        Console.WriteLine("0. Выйти");
+                        Console.Write("Выберите действие: ");
+                        string choice3 = Console.ReadLine();
+
+                        switch (choice3)
+                        {
+                            case "1":
+                                RussianFrenchDictionary.AddWordTranslation();
+                                break;
+                            case "2":
+                                RussianFrenchDictionary.ReplaceWordTranslation();
+                                break;
+                            case "3":
+                                RussianFrenchDictionary.DeleteWordTranslation();
+                                break;
+                            case "4":
+                                RussianFrenchDictionary.SearchTranslation();
+                                break;
+                            case "5":
+                                RussianFrenchDictionary.ExportWords();
+                                break;
+                            case "0":
+                                exit3 = true;
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                                break;
+                        }
+                    }
+                    break;
+                case "4":
+
+                    FrenchRussianDictionary FrenchRussianDictionary = new FrenchRussianDictionary();
+                    bool exit4 = false;
+                    while (!exit4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("===  Французко-русский словарь ===");
+                        Console.WriteLine("1. Добавить слово и перевод");
+                        Console.WriteLine("2. Заменить слово или перевод");
+                        Console.WriteLine("3. Удалить слово или перевод");
+                        Console.WriteLine("4. Найти перевод слова");
+                        Console.WriteLine("5. Экспорт слов и переводов");
+                        Console.WriteLine("0. Выйти");
+                        Console.Write("Выберите действие: ");
+                        string choice4 = Console.ReadLine();
+
+                        switch (choice4)
+                        {
+                            case "1":
+                                FrenchRussianDictionary.AddWordTranslation();
+                                break;
+                            case "2":
+                                FrenchRussianDictionary.ReplaceWordTranslation();
+                                break;
+                            case "3":
+                                FrenchRussianDictionary.DeleteWordTranslation();
+                                break;
+                            case "4":
+                                FrenchRussianDictionary.SearchTranslation();
+                                break;
+                            case "5":
+                                FrenchRussianDictionary.ExportWords();
+                                break;
+                            case "0":
+                                exit4 = true;
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                                break;
+                        }
+                    }
+                    break;
+                case "0":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ RussianEnglishDictionary.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianEnglish
+{
+    class RussianEnglishDictionary
+    {
+        private Dictionary<string, List<string>> dictionary;
+
+        public RussianEnglishDictionary()
+        {
+            dictionary = new Dictionary<string, List<string>>();
+        }
+
+        public void AddWordTranslation()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Добавление слова и перевода ===");
+            Console.Write("Введите слово на русском: ");
+            string word = Console.ReadLine();
+
+            Console.Write("Введите перевод слова на английский: ");
+            string translation = Console.ReadLine();
+
+            if (dictionary.ContainsKey(word))
+            {
+                dictionary[word].Add(translation);
+            }
+            else
+            {
+                dictionary.Add(word, new List<string> { translation });
+            }
+
+            Console.WriteLine("Слово и перевод успешно добавлены.");
+
+            using (StreamWriter writer = new StreamWriter("русско - английский словарь.txt", true))
+            {
+                writer.WriteLine("Слово: {0}", word);
+                writer.WriteLine("Перевод: {0}", translation);
+                writer.WriteLine();
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+
+        public void ReplaceWordTranslation()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Замена слова или перевода ===");
+            Console.Write("Введите слово, которое нужно заменить: ");
+            string word = Console.ReadLine();
+
+            if (dictionary.ContainsKey(word))
+            {
+                Console.Write("Введите новое слово на русском: ");
+                string newWord = Console.ReadLine();
+
+                Console.Write("Введите новый перевод слова на английском: ");
+                string newTranslation = Console.ReadLine();
+
+                List<string> translations = dictionary[word];
+
+                dictionary.Remove(word);
+
+                dictionary.Add(newWord, translations);
+
+                Console.WriteLine("Слово или перевод успешно заменены.");
+                List<string> fileLines = File.ReadAllLines("русско - английский словарь.txt").ToList();
+                fileLines.RemoveAll(line => line.StartsWith("Слово: " + word) || line.StartsWith("Перевод:"));
+                fileLines.Add("Слово: " + newWord);
+                fileLines.Add("Перевод: " + newTranslation);
+                fileLines.Add("");
+                File.WriteAllLines("русско - английский словарь.txt", fileLines);
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено.");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+
+
+
+        public void DeleteWordTranslation()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Удаление слова или перевода ===");
+            Console.Write("Введите слово, которое нужно удалить: ");
+            string word = Console.ReadLine();
+
+            if (dictionary.ContainsKey(word))
+            {
+                dictionary.Remove(word);
+                Console.WriteLine("Слово и его переводы успешно удалены.");
+
+                string[] fileLines = File.ReadAllLines("русско - английский словарь.txt");
+
+                List<string> updatedFileLines = new List<string>();
+
+                bool removeNextLine = false;
+
+                foreach (string line in fileLines)
+                {
+                    if (removeNextLine)
+                    {
+                        removeNextLine = false;
+                        continue;
+                    }
+
+                    if (line.StartsWith("Слово: " + word))
+                    {
+                        removeNextLine = true;
+                        continue;
+                    }
+
+                    updatedFileLines.Add(line);
+                }
+
+                File.WriteAllLines("русско - английский словарь.txt", updatedFileLines);
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено.");
+            }
+
+
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+
+        public void SearchTranslation()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Поиск перевода ===");
+            Console.Write("Введите слово на русском для поиска перевода: ");
+            string word = Console.ReadLine();
+
+            if (dictionary.ContainsKey(word))
+            {
+                List<string> translations = dictionary[word];
+
+                Console.WriteLine("Перевод(ы) слова '{0}':", word);
+                foreach (string translation in translations)
+                {
+                    Console.WriteLine("- {0}", translation);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено.");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+
+        public void ExportWords()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Экспорт слов и переводов ===");
+            Console.Write("Введите имя файла для экспорта: ");
+            string fileName = Console.ReadLine();
+
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine("Файл с таким именем уже существует. Перезаписать? (y/n)");
+                string overwriteChoice = Console.ReadLine();
+
+                if (overwriteChoice.ToLower() != "y")
+                {
+                    Console.WriteLine("Экспорт отменен.");
+                    Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (KeyValuePair<string, List<string>> entry in dictionary)
+                {
+                    string word = entry.Key;
+                    List<string> translations = entry.Value;
+
+                    writer.WriteLine("Слово: {0}", word);
+                    writer.WriteLine("Переводы:");
+                    foreach (string translation in translations)
+                    {
+                        writer.WriteLine(" {0}", translation);
+                    }
+                    writer.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Экспорт завершен. Файл сохранен: {0}", fileName);
+
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+    }
+}
